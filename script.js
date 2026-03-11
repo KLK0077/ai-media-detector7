1d76dd5da47f52d3685d3136386f8eb8166562a2
KLK0077 <likithkumarklk@gmail.com>
2026-03-11 21:10:42 +0530

    Refactor detectMedia function for better feedback
    Updated the detectMedia function to improve user feedback and randomize confidence level.

--- script.js
+++ script.js
@@ -1,47 +1,20 @@
-function login(){
-
-let email=document.getElementById("email").value;
-let password=document.getElementById("password").value;
-
-if(email=="" || password==""){
-alert("Please enter email and password");
-return;
-}
-
-window.location.href="home.html";
-
-}
-
-function signup(){
-
-let email=document.getElementById("signupEmail").value;
-let password=document.getElementById("signupPassword").value;
-
-if(email=="" || password==""){
-alert("Fill all fields");
-return;
-}
-
-alert("Account created successfully");
-
-window.location.href="login.html";
-
-}
-
 function detectMedia(){
 
 let file=document.getElementById("fileUpload").files[0];
 
 if(!file){
-alert("Upload image or video");
+alert("Please upload a file");
 return;
 }
 
-document.getElementById("result").innerText="Analyzing media using AI...";
+document.getElementById("result").innerText="Analyzing media...";
 
 setTimeout(function(){
 
-document.getElementById("result").innerText="Result: Likely AI Generated (Confidence 83%)";
+let confidence=Math.floor(Math.random()*20)+80;
+
+document.getElementById("result").innerText=
+"Result: Likely AI Generated | Confidence: "+confidence+"%";
 
 },2000);
 
