49f93d48db3a356e849d4161d4ab717318a8ef64
KLK0077 <likithkumarklk@gmail.com>
2026-03-11 21:25:34 +0530

    Refactor detectMedia function for better file handling
    Updated detectMedia function to improve file upload alert.

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-function login(){
-
-let email=document.getElementById("email").value;
-let password=document.getElementById("password").value;
-
-if(email=="" || password==""){
-alert("Please enter email and password");
-return;
-}
-
-window.location.href="home.html";
-
-}
-
-function signup(){
-
-let email=document.getElementById("signupEmail").value;
-let password=document.getElementById("signupPassword").value;
-
-if(email=="" || password==""){
-alert("Fill all fields");
-return;
-}
-
-alert("Account created successfully");
-
-window.location.href="index.html";
-
-}
-
 function detectMedia(){
 
 let file=document.getElementById("fileUpload").files[0];
 
 if(!file){
-alert("Upload image or video");
+alert("Please upload a file");
 return;
 }
 
